137b01f027e6f2ddf57638641232ef5dd9094695
waseem salahat <waseemsalahat11@gmail.com>
2026-03-05 20:18:05 +0200

    Add files via upload
    Mobile Hybrid Search Shell App

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView 
+        source={{ uri: 'https://www.google.com' }} 
+        style={styles.webview}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  webview: {
+    flex: 1,
+  },
+});
